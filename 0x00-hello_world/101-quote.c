35a383c612c5184fdde6ced80232cfc8fba4fbaf
OmarSoliman74 <mingosos13@gmail.com>
2023-03-17 11:21:33 -0700

    3

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that print a line to the standard error
+ * Return 1 (Success)
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora kor par, 2023-3-17\n", stdout);
+	return (1);
+}
